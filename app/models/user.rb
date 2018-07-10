a1836767cebad0ca731874adf1dc94d4a28a9c37
amckean12 <amckean@me.com>
2018-07-10 15:04:42 -0500

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  #User Validations
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :height, presence: true
+  validates :weight, presence: true
+  validates :email, :uniqueness => { :case_sensitive => false }
+
+  has_secure_password
 end
